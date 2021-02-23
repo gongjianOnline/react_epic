519d8d351c28bc53ce0b55f7bee2de349997d287
李承斌 <15031617367@163.com>
2021-02-23 17:22:05 +0800

    页面架构搭建

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import {
+  Switch,
+  Route
+} from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import History from "./pages/History"
+import About from "./pages/About"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/history' component={History}/>
+        <Route path='/about' component={About}/>
+      </Switch>
+
+      <Footer></Footer>
     </div>
   );
 }
