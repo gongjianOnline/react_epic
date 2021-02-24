f6ca36e61703656f8337fe388418a44695d02482
李承斌 <15031617367@163.com>
2021-02-24 09:43:10 +0800

    实现路由懒加载

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from "react"
-import {
-  Switch,
-  Route
-} from "react-router-dom"
+import React,{Suspense,lazy} from "react"
+import {Switch, Route} from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Loading from "./components/Loading"
 
-import Home from "./pages/Home"
-import History from "./pages/History"
-import About from "./pages/About"
+// import Home from "./pages/Home"
+// import History from "./pages/History"
+// import About from "./pages/About"
+const Home = lazy(()=>import('./pages/Home'))
+const History = lazy(()=>import('./pages/History'))
+const About = lazy(()=>import('./pages/About'))
+
+
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Switch>
-        <Route path='/' exact component={Home}/>
-        <Route path='/history' component={History}/>
-        <Route path='/about' component={About}/>
-      </Switch>
-
+      <Suspense fallback={<Loading/>}>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/history' component={History}/>
+          <Route path='/about' component={About}/>
+        </Switch>
+      </Suspense>
       <Footer></Footer>
     </div>
   );
