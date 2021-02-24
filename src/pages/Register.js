32a95bddc9877a7eb418d26d5d7c9eaa5f040d7c
李承斌 <15031617367@163.com>
2021-02-24 15:21:10 +0800

    重构注册页面,使用antd组件

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,78 @@
 import React from "react"
+import { Form, Input, Button, Checkbox } from 'antd';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0,0,0,0.2);
+  border-radius:4px;
+  padding:20px;
+`
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`
+
+
 function Register(){
+    const layout = {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 20 },
+    };
+    const tailLayout = {
+      wrapperCol: { offset: 4, span: 20 },
+    };
+    const onFinish = (values) => {
+      console.log('Success:', values);
+    };
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Wrapper>
+      <Title>注册</Title>
+      <Form
+        {...layout}
+        name="basic"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[{ required: true, message: '请输入用户名！' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: '请输入密码！' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="确认密码"
+          name="affPassword"
+          rules={[{ required: true, message: '请输入确认密码！' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>记住密码</Checkbox>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            注册
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
+
   )
 }
 export default Register
