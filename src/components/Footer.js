be460b29a358358710cef9406b4b8bfeb67503d9
李承斌 <15031617367@163.com>
2021-02-24 10:58:35 +0800

    页面样式完成

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react"
+import styled from "styled-components"
+const StyledFooter = styled.footer`
+  padding: 10px 100px;
+  text-align: center;
+  font-size: 12px;
+  color:#aaa
+`
 function Footer(){
   return(
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <StyledFooter>
+      <div>Footer</div>
+    </StyledFooter>
   )
 }
 export default Footer
